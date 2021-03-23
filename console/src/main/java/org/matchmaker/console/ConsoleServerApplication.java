6f5bd14ea003a98dd63b49db5b2d9a3e49c74902
mliu <mliuzhongshuai@163.com>
2021-03-23 15:36:15 +0800

    -broker指标获取

--- console/src/main/java/org/matchmaker/console/ConsoleServerApplication.java
+++ console/src/main/java/org/matchmaker/console/ConsoleServerApplication.java
@@ -3,6 +3,9 @@ package org.matchmaker.console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author liuzhongshuai
+ */
 @SpringBootApplication
 public class ConsoleServerApplication {
 
